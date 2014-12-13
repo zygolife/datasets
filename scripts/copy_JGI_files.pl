c656e1c0a4f563606c1e3465bbcb530df10431d0
Jason Stajich <jason@bioperl.org>
2014-12-13 14:34:01 -0800

    make the prefix file for lookups -- works for all files not just JGI files

--- scripts/copy_JGI_files.pl
+++ scripts/copy_JGI_files.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Getopt::Long;
 my $debug =0;
 my $debug_one = 0;
 my $force = 0;
+
+GetOptions('f|force!' => \$force,
+	   'v|debug!' => \$debug,
+	   'one!'     => \$debug_one,
+    );
 my $dir = shift || "download";
 my $p_odir = shift || 'genomes/pep';
 my $g_odir = shift || 'genomes/GFF';
@@ -14,33 +20,62 @@ for $dir ( $p_odir, $g_odir, $d_odir, $c_odir) {
  mkdir $dir;
 }
 opendir(DIR, $dir) || die $!;
+open(my $lookup => ">lookup_prefix.dat") || die $!;
+my %prefix;
 for my $f ( readdir(DIR) ) {
     next if $f =~ /^\./;
     opendir(FAM,"$dir/$f") || die $!;
-    for my $sp ( readdir(FAM) ) {
+    for my $sp ( readdir(FAM) ) {	
 	next if $sp =~ /^\./;
 	next if ( ! -d "$dir/$f/$sp");
 	warn("sp is $dir/$f/$sp\n") if $debug;
 	opendir(SP,"$dir/$f/$sp") || die "$dir/$f/$sp $!";
+	$sp =~ s/var\./var/;
+	my ($stem1) = split(/\./,$sp);
+#	my @n = split(/_/,$stem);
+#	$stem =~ s/var\.?_\w+//;
+	my ($g,$species,$rest) = split(/_/,$stem1,3);
+	my $prefix = substr($g,0,1) . substr($species,0,3);
+	if( $rest ) {
+	    $rest =~ s/-/_/;
+	    $rest =~ s/_#//;
+	    my @n = split(/_/,$rest);
+	    if( $n[0] eq 'var' ) {
+		shift @n;
+		shift @n;
+	    }
+	    $rest = join("_",@n);
+	    $prefix .= "_$rest";
+	}
+	warn("g is $g species is $species -- $stem1 -- $prefix\n");
+	$prefix{$stem1} = $prefix;
 	for my $file ( readdir(SP) ) {
 	    if( $file =~ /(\S+\.aa\.fasta).gz$/) {
 		my $stem = $1;
-		next if -f "$p_odir/$stem";
+		next if ! $force && -f "$p_odir/$stem";
 		print("zcat $dir/$f/$sp/$file | ",
 		      'perl -p -e \'s/^>(\w+)\|(\S+)\|(\d+)\|(\S+)/>$2|$2_$3 $4/\' > ',"$p_odir/$stem\n");
             } elsif ( $file =~ /(\S+\.CDS\.fasta).gz$/) {
 		my $stem = $1;
-                next if -f "$c_odir/$stem";
+                next if !$force && -f "$c_odir/$stem";
                 print("zcat $dir/$f/$sp/$file | ",
                       'perl -p -e \'s/^>(\w+)\|(\S+)\|(\d+)\|(\S+)/>$2|$2_$3 $4/\' > ',"$c_odir/$stem\n");
 	    } elsif ( $file =~ /(\S+\.assembly\.fasta).gz$/) {
 		my $stem = $1;
+		my (@all) = split(/\./,$stem);
+		my $jgiprefix = $all[-4];
+		$prefix{$stem1} = $jgiprefix;
 		$stem =~ s/\.assembly//;
-		next if -f "$d_odir/$stem";
-		print("zcat $dir/$f/$sp/$file > $d_odir/$stem\n");
+		next if !$force && -f "$d_odir/$stem";
+		print("zcat $dir/$f/$sp/$file | perl -p -e 's/^>/>$jgiprefix|/' > $d_odir/$stem\n");
 	    } elsif ( $file =~ /(\S+\.gff3).gz$/) {
 		my $stem = $1;
+		my (@all) = split(/\./,$stem);
+		my $jgiprefix = $all[-3];
+		$prefix{$stem1} = $jgiprefix;
+
 		next if !$force && -f "$g_odir/$stem";
+		
 		open(my $in => "zcat $dir/$f/$sp/$file |") || die "cannot open $dir/$f/$sp/$file: $!";
 		open(my $out => ">$g_odir/$stem")|| die "cannot open $g_odir/$stem: $!";
 		while(<$in>) {
@@ -48,6 +83,7 @@ for my $f ( readdir(DIR) ) {
 		    if( ! /^\#/ ) {
 			chomp;
 			my @row = split(/\t/,$_);
+			$row[0] = join("_",$prefix{$stem1}, $row[0]);
 			my $last = pop @row;
 			my (@order,%ninth);
 			for my $ent ( split(/;/,$last) ) {
@@ -76,4 +112,8 @@ for my $f ( readdir(DIR) ) {
 	}
 #	last if $debug_one;
     }
+}
+
+for my $stem ( sort keys %prefix ){
+    print $lookup join("\t", $prefix{$stem}, $stem),"\n";
 }
