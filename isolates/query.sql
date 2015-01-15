28d8c8b2b9a7c34c896443187bd77d898d617a6f
Jason Stajich <jason.stajich@ucr.edu>
2015-01-14 21:52:37 -0800

    make csv dbs as output so they render on github.com nicely

--- isolates/query.sql
+++ isolates/query.sql
@@ -1,14 +1,14 @@
 .header on
 .mode csv
-.output "query_results/genus_species_count.txt"
+.output "query_results/genus_species_count.csv"
 select genus, species, count(*) from isolate group by genus,species;
 
-.output "query_results/genus_count.txt"
+.output "query_results/genus_count.csv"
 select genus, count(*) as count from isolate group by genus;
 
-.output "query_results/type_strain.txt"
+.output "query_results/type_strain.csv"
 select genus,species,subspecies,collection,strain_id from isolate where type_strain='TRUE';
 
-.output "query_results/genus_genomes_sampled.txt"
+.output "query_results/genus_genomes_sampled.csv"
 select isolate.genus, count(genomes.status) > 0 as any_genomes_sampled
 FROM isolate LEFT OUTER JOIN genomes ON isolate.genus=genomes.genus group by isolate.genus;
