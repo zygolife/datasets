460e83c3195bc64b99d1fe77fcd085b8d67ac3e9
Jason Stajich <jason.stajich@ucr.edu>
2015-01-14 21:13:40 -0800

    update queries for reporting

--- isolates/query.sql
+++ isolates/query.sql
@@ -1,3 +1,8 @@
 .header on
 .mode csv
+.output "query_results/genus_species_count.txt"
 select genus, species, count(*) from isolate group by genus,species;
+.output "query_results/genus_count.txt"
+select genus, count(*) as count from isolate group by genus;
+.output "query_results/type_strain.txt"
+select genus,species,subspecies,collection,strain_id from isolate where type_strain='TRUE';
