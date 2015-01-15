3d9e8a354736174fa9d116c5a855d4107f1495c2
Jason Stajich <jason.stajich@ucr.edu>
2015-01-14 21:41:09 -0800

    cross ref with genome table to see who is seq'n

--- isolates/query.sql
+++ isolates/query.sql
@@ -2,7 +2,13 @@
 .mode csv
 .output "query_results/genus_species_count.txt"
 select genus, species, count(*) from isolate group by genus,species;
+
 .output "query_results/genus_count.txt"
 select genus, count(*) as count from isolate group by genus;
+
 .output "query_results/type_strain.txt"
 select genus,species,subspecies,collection,strain_id from isolate where type_strain='TRUE';
+
+.output "query_results/genus_genomes_sampled.txt"
+select isolate.genus, count(genomes.status) > 0 as any_genomes_sampled
+FROM isolate LEFT OUTER JOIN genomes ON isolate.genus=genomes.genus group by isolate.genus;
